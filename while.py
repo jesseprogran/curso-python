b81d33fcc996efa74528c8345712432004801127
Jesse Silva <jessepescador22@gmail.com>
2024-05-09 21:37:02 -0300

    aprendendo sobre o while

---
+++ while.py
@@ -0,0 +1,9 @@
+nome = None
+
+while True:
+    print('Digite seu nome, ou x para parar:')
+    nome = input()
+    if nome == 'x' or nome == 'x':
+        break
+    print(f'Bem Vindo, {nome}')
+print('Até logo!')    
