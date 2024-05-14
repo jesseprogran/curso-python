68fb4b8478d947bd05ea1106e74c23bd50e53465
Jesse Silva <jessepescador22@gmail.com>
2024-05-14 20:36:44 -0300

    praticando

--- print.py
+++ print.py
@@ -1,19 +1,9 @@
-#Sintaxe:
-#print(objetos, argumentos)
+nome = str(input('Digite seu nome: '))
+idade = int(input('digite sua idade: '))
+peso = float(input('digite seu peso: '))
+estudante = bool(input('você está estudando: '))
 
-nome = input('Digite seu nome: ')
-msg = 'Olá ' + nome + ', Seja bem vindo ao curso de pyuthon.'
-print(msg)
-
-name = 'Jessé'
-age = 33
-msg_formatada = 'o nome dele é {0} e ele tem {1} anos'.format(name,age)
-print(msg_formatada)
-
-
-
-peso = 111
-altura = 1.75
-
-mensagem = f'Olá, me chamo {name}, tenho {age} anos meu peso atual é de {peso}kg e minha atura é de {altura}'
-print(mensagem)
+print(nome)
+print(idade)
+print(peso)
+print(estudante)
