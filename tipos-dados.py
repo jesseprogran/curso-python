3ba605743dc3f2f7a0c69ae28332ec2a2c21ee41
Jesse Silva <jessepescador22@gmail.com>
2024-05-14 20:21:13 -0300

    realizando e avançando nos estudos

---
+++ tipos-dados.py
@@ -0,0 +1,35 @@
+"""
+    Tipos de Dados
+    
+    int - inteiro
+    float - decimal
+    str - string
+    bool - boolean (true ou false)
+    list - lista
+    tuple - tupla
+    dict - dicionário
+    
+"""    
+
+
+nome = 'jessé'
+idade = 32
+peso = 111
+altura = 1.74
+estudante = True
+
+lista = [nome, idade, peso, altura, estudante] #mutáveis
+tupla = ('José', 23, 123, 1.43, 'estudante')#são imutáveis
+
+print(lista)
+print(tupla)
+
+dicionario = {
+    'nome':"Diego",
+    'idade': 32,
+    'altura': 1.74,
+    'peso': 111,
+    'estudante':False
+}
+
+print(dicionario)
