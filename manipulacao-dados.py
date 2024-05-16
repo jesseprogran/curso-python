b86339d0229c428f01d2d233a05e1af6a9378025
Jesse Silva <jessepescador22@gmail.com>
2024-05-16 19:35:59 -0300

    avançando no conteudo

---
+++ manipulacao-dados.py
@@ -0,0 +1,49 @@
+#append(item)
+# lista = [1,2,3]
+# print(lista)
+
+# lista.append(4)
+# print(lista)
+#sort()
+# lista = [1,2,3,10,23,12,14,15]
+# print(lista)
+
+# lista.sort()
+# print(lista)
+#insert(i, v)
+# lista = [1,2,3]
+# print(lista)
+
+# lista.insert(3,4)
+# print(lista)
+#extend(lista)
+# lista = [1,2,3]
+# print(lista)
+# lista2 = [12,23,14,123]
+
+# lista.extend(lista2)
+# print(lista)
+#remove(item) / del lista[i]
+# lista = [1,2,3]
+# print(lista)
+
+# lista.remove(1)
+# del lista[2]
+# print(lista)
+#index(item)
+# lista = [1,2,3]
+# print(lista)
+
+# print(lista.index(2))
+# print(lista)
+
+#min / max / sum /
+lista = [1,2,3]
+print(lista)
+
+lista.append(4)
+print(lista)
+
+print(min(lista))
+print(max(lista))
+print(sum(lista))
