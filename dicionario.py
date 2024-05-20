7308a7b076618d9b82309c849861a481b895d253
Jesse Silva <jessepescador22@gmail.com>
2024-05-20 20:29:12 -0300

    realizando estudo sobre dicionario

---
+++ dicionario.py
@@ -0,0 +1,13 @@
+pessoa = {
+    'nome': 'Jessé Silva',
+    'idade':32,
+    'cidade': 'Icapuí'
+}
+
+print(pessoa['nome'])
+
+
+
+carro = [{'nome':'Gol', 'ano':1991, 'fabrica':'CE'}, {'nome':'Fiat', 'ano':2001, 'fabrica':'RS'}]
+
+print(carro[1]['nome'])
