aa4accb8ebf5c2ebe1079dcb68458f32864624d8
Jesse Silva <jessepescador22@gmail.com>
2024-05-20 21:03:49 -0300

    funções

---
+++ funcoes.py
@@ -0,0 +1,14 @@
+def soma(n1, n2):
+    resultado = n1 + n2
+    return resultado
+
+def multiplica(num, num2):
+    res = num * num2
+    return res
+
+
+somaTotal = soma(10, 23)
+multiTotal = multiplica(21, 12)
+
+print(somaTotal)
+print(multiTotal)
