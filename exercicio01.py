c717bf5df3436216d9f85d52b37ff5cc278f04b1
Jesse Silva <jessepescador22@gmail.com>
2024-05-22 23:31:38 -0300

    resolvendo exercico

--- exercicio01.py
+++ exercicio01.py
@@ -12,4 +12,23 @@
 # print(f'a média final foi {media}')
 
 
-#02- 
+#02- descubra o servidor do email, pegar o 1 nome do usuario,
+# construa uma mensagem: Usuario primeiro_nome cadastrado com sucesso e o email tal
+# construa uma mensagem: Enviamos um link de confirmação para o email j***@gmail.com
+nome = 'Jessé Pereira da Silva'
+email = 'jessecode22@gmail.com'
+
+posicao = email.find('@')
+servidor = email[posicao:]
+print(servidor)
+
+posicao = nome.find(' ')
+primeiro_nome = nome[:posicao]
+print(primeiro_nome)
+
+mensagem = f' Usuário {primeiro_nome} cadastrado com sucesso com o email {email}'
+print(mensagem)
+
+primeira_letra = email[0]
+mensagem2 = f'Enviamos um link de confirmação para o email {primeira_letra}***{servidor}'
+print(mensagem2)
